c31b332d06614f5d0dd437acdbd2e3e226c416d3
m-ghozi <ghozi286@gmail.com>
2024-12-13 09:36:52 +0700

    🚀 init repo

---
+++ openbox/themes/gruvbox/theme.bash
@@ -0,0 +1,92 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2024 Aditya Shakya <adi1090x@gmail.com>
+#
+# Forest Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#282828'
+foreground='#eadab2'
+color0='#282828'
+color1='#cc241d'
+color2='#98971a'
+color3='#d79921'
+color4='#458588'
+color5='#b16286'
+color6='#689d6a'
+color7='#a89984'
+color8='#928374'
+color9='#fb4934'
+color10='#b8bb26'
+color11='#fabd2f'
+color12='#83a598'
+color13='#d3869b'
+color14='#8ec07c'
+color15='#eadab1'
+
+accent='#c4c7c5'
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='Iosevka:style=bold:size=10;3'
+
+# Rofi
+rofi_font='Iosevka 10'
+rofi_icon='Papirus-Apps'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='gruvbox.conf'
+geany_font='JetBrainsMono Nerd Font 10'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='Gruvbox'
+icon_theme='Vimix-White-Dark'
+cursor_theme='Future-Dark'
+
+# Openbox
+ob_theme='OB-Gruvbox-Mod'
+ob_layout='LIMC'
+ob_font='JetBrains Mono'
+ob_font_size='9'
+ob_menu='menu-icons.xml'
+ob_margin_t='40'
+ob_margin_b='10'
+ob_margin_l='10'
+ob_margin_r='10'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x40'
+dunst_origin='top-right'
+dunst_font='Iosevka Custom 9'
+dunst_border='0'
+dunst_separator='2'
+
+# Plank
+plank_hmode='auto'
+plank_offset='95'
+plank_position='right'
+plank_theme='Transparent'
+plank_icon_size='32'
+plank_zoom_percent='120'
+
+# Picom
+picom_backend='glx'
+picom_corner='10'
+picom_shadow_r='20'
+picom_shadow_o='0.60'
+picom_shadow_x='-20'
+picom_shadow_y='-20'
+picom_blur_method='none'
+picom_blur_strength='0'
